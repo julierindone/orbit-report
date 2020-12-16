f11139075d9a2d8902e2a0d41fd30731e492998c
Julie Rindone <julie.rindone@gmail.com>
2020-12-16 09:19:28 -0600

    What's done so far - Assign 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
-
- 
   sourceList: Satellite[];
 
 
-  constructor() {
-    this.sourceList = [
-      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-      new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-      new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-   ];
+  constructor(){
+    this.sourceList = [];
+    //this is where it starts to differ from the earlier version.
+    let satellitesURL = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesURL).then(function(response) {
+      response.json().then(function(data){
+
+    let fetchedSatellites = data.satellites;
+            
+        for (let i = 0; i < fetchedSatellites.length; i++) {
+
+          //this.sourceList.push(satellite) = [
+          //  new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          return console.log(fetchedSatellites[i].name)
+          //]
+        }
+        }.bind(this));    
+    }.bind(this));
   }
 }
+
+
+          //DONE////// TODO: loop over satellites
+
+         // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+         // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+      //console.log(fetchedSatellites)
+      //console.log(fetchedSatellites[0])
+
+
 
